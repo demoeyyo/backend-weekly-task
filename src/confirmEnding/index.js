8fb8c09beb09e8d3b52f642328ce6c70bf31b354
demoeyyo <85228423+demoeyyo@users.noreply.github.com>
2021-06-16 03:42:01 -0700

    Delmas week2 task

--- src/confirmEnding/index.js
+++ src/confirmEnding/index.js
@@ -1,6 +1,11 @@
 function confirmEnding(str, target) {
+    if (str.endsWith(target)){
+        return true;
+    }else{
+        return false
+    }
+  }
   
-}
 
-confirmEnding("Bastian", "n") // => true
+alert (confirmEnding("Bastian", "n")) // => true
 confirmEnding("Congratulation", "on") // => true
