d2a064a6d4712d8e5526881b9d37e20f015fec5d
demoeyyo <85228423+demoeyyo@users.noreply.github.com>
2021-06-21 02:56:20 -0700

    weekly task

--- src/delmas-oyibo/evenlySpaced.js
+++ src/delmas-oyibo/evenlySpaced.js
@@ -1,7 +1,10 @@
 
 function evenlySpaced(first, second, last) {
     //Provide your solution here
-    
+    if (first > second && first > last && second > last){
+        return first - second == second - lastl;
+    }
+    return last - second == second - first;
 }
 
 evenlySpaced(2,4,6) // -> should return true
